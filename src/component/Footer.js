7501a69cb1a657366d4051f20fc070fede1d1882
My Name <myemail@example.com>
2024-04-15 19:05:30 +0530

    last

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,22 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Itemcontainer from './items/Itemcontainer'
 import ScrollToTop from './Scrolltotop';
 import cogoToast from 'cogo-toast';
+import axios from 'axios';
 
 const Footer = () => {
+  const [email ,setEmail] = useState("");
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, []);
-  const subscribeHandler = () => {
-    cogoToast.success("Form Submitted Successfully")
+  const subscribeHandler = async (e) => {
+    try {
+      const { data } = await axios.post(
+        `https://restapi.ansoftt.com:4321/v1/subscribe/`,
+        {
+          email : email,
+         
+        }
+      );
+  
+      cogoToast.success("Form Submitted Successfully")
+      
+    } catch (error) {
+      cogoToast.error("Already Subscribed", { position: "bottom-left" });
+      
+    }
 
   }
+  
   return (
     <footer className='bg-gray-700 text-white relative'>
       <div className='h-50 md:flex md:justify-around md:items-center sm:px-12 px-4 bg-customOrange py-7'>
-        <h1 className='lg:text-3xl text-xl md:mb-0 mb-6 lg:leadiing-normal font-semibold md:w-2/5'>Subscribe Our Newsletter</h1>
+        <h1 className='lg:text-3xl text-xl md:mb-0 mb-6 lg:leadiing-normal font-semibold md:w-2/5'>Subscribe For Latest Products</h1>
         <div>
-          <input type='text' placeholder='Enter your E-mail' className='text-gray-400 sm:w-72 w-full  sm:mr-5 mr-1 lg:mb-0 py-2.5 rounded px-2 focus:outline-none' />
+          <input type='text' placeholder='Enter your E-mail'  value={email} onChange= {(e) => setEmail(e.target.value)}className='text-gray-400 sm:w-72 w-full   sm:mr-5 mr-1 lg:mb-0 py-2.5 rounded px-2 focus:outline-none' />
           <button className='bg-gray-600 hover:bg-gray-400 px-5 py-2.5 font-poppins rounded-md text-white ' onClick={subscribeHandler}>Subscribe</button>
         </div>
 
--- src/component/items/Items.js
+++ src/component/items/Items.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Items = ({ Links, title }) => {
+  const navigate = useNavigate();
   return (
     <ul className="">
       <h1 className="mb-4 font-semibold">{ }</h1>
       {Links.map((link) => (
         <li key={link.name}>
-         <Link to={'/'}> <img src={link.image} style={{ width: "7rem", height: "7rem" }} /></Link>
+          <img onClick={() => navigate("/")} className="cursor-pointer" src={link.image} style={{ width: "7rem", height: "7rem" }} />
           <div className="flex  justify-start gap-2 mt-1 flex-col">
             <div className="pr-1">
               <a href={'https://play.google.com/store/apps/details?id=com.anintl.automeka&pli=1'} target="_blank" rel="noopener noreferrer"><img src={link.image1} style={{ width: "6rem", height: "2rem" }} /></a>
